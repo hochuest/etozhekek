5bde36826535a3a27657c8a0a49f5bef9454f9df
hochuest <istatenin@bk.ru>
2018-10-25 15:48:31 +0400

    start

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {add, multiply} from './math';
+import { t } from 'three';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
+
+let num1 = 5, num2 = 10;
+
+alert('Add: '+ add(num1, num2));
+console.log('Multiply: ', multiply(num1, num2));
